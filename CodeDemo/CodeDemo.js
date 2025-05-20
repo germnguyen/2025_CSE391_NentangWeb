cce61c76ed4cb8df9cb4015428730dbf955bccae
DucNguyen <ducnguyen2005105@gmail.com>
2025-05-20 20:47:01 +0700

    them demo chuyen doi cu phap JSX

---
+++ CodeDemo/CodeDemo.js
@@ -0,0 +1,39 @@
+// cơ chế chuyển đổi JSXJSX
+// Code JSX
+    function formatName(user){
+    return user.firstName + ' ' + user.lastName;
+    }
+    const users = {
+    firstName: 'Nguyen', 
+    lastName: 'Son'
+    }
+
+    function getGreeting(users){
+    if(userss){
+        return <h1> Hello, {formatName(user)}</h1>
+    }
+    return <h1>Hello, stranger.</h1>
+    }
+
+    console.log(getGreeting(users));
+
+// Chuyển về JS
+    import { jsxs as _jsxs, jsx as _jsx } from "react/jsx-runtime";
+    function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+    }
+    const user = {
+    firstName: 'Nguyen',
+    lastName: 'Son'
+    };
+    function getGreeting(user) {
+    if (user) {
+        return /*#__PURE__*/_jsxs("h1", {
+        children: [" Hello, ", formatName(user)]
+        });
+    }
+    return /*#__PURE__*/_jsx("h1", {
+        children: "Hello, stranger."
+    });
+    }
+    console.log(getGreeting(user));
